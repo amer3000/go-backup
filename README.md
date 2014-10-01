6eda8691faa05a1bd7b3cd99a258d28873b8ee69
Amer Khalid <amer@amerkhalid.com>
2014-09-30 20:21:24 -0500

    Playing with hashing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 go-backup
 =========
+
+Commandline for now
+uses config file in yaml
+The options are : back up paths, backup frequency, encryption, compression, backup server location (ftp, s3, SoftLayer Object Storage), backup run time,
+
+Hash file fields: file full path, hash, timestamp of hash,  
+
--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
 )
 
+//https://github.com/mrjbq7/re-factor/blob/master/dupe/dupe.go
+
+var rootDir string = "."
+
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Hello World!!!!!")
+	t0 := time.Now()
+	filepath.Walk(rootDir, VisitFile)
+	t1 := time.Now()
+	fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
+}
+
+func MD5OfFile(fullpath string) []byte {
+	if contents, err := ioutil.ReadFile(fullpath); err == nil {
+		md5sum := md5.New()
+		md5sum.Write(contents)
+		return md5sum.Sum(nil)
+	}
+	return nil
+}
+
+func VisitFile(fullpath string, f os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	if !f.IsDir() {
+		hash := MD5OfFile(fullpath)
+		fmt.Printf("%s    %x\n", fullpath, hash)
+	}
+	return nil
 }
